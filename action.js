8a763f51c98f629800e68d348781da1a859e84ae
Cristian Ventura <cristian101ventura@yahoo.com>
2016-12-17 00:21:47 -0500

    Current and new records can be displayed

--- action.js
+++ action.js
@@ -2,19 +2,37 @@
 var button = document.getElementById("add"),
     first = document.getElementById("firstname"),
     last = document.getElementById("lastname"),
-    email = document.getElementById("email");
+    age = document.getElementById("age"),
+    display = document.getElementById("display-records");
 
 var allRecords = [];
 
-var Record = function Record(first, last, email){
+var Record = function Record(first, last, age){
     this.firstname = first;
     this.lastname = last;
-    this.emial = email;
+    this.age = age;
 };
 
+var record1 = new Record("Cristian", "Ventura", "22"),
+    record2 = new Record("Joe", "Rogan", "34");
 
+allRecords.push(record1);
+allRecords.push(record2);
+
+var btn = "<td><button class=\"btn btn-xs btn-danger\"> Delete <\/button> <\/td>";
+
+for (var x = 0; x < allRecords.length; x++){
+display.innerHTML += "<tr><td>" + allRecords[x].firstname +"<\/td><td>" + allRecords[x].lastname + "<\/td>" + "<\/td><td>" + allRecords[x].age + btn + "<\/tr>";
+};
 
 button.addEventListener("click", function test(){
-    allRecords.push(new Record(first.value, last.value, email.value))
-    alert(allRecords[0].firstname + " " + allRecords[0].lastname + " " + allRecords[0].emial);
+   allRecords.push( new Record(first.value, last.value, age.value)); 
+
+   for (var x = allRecords.length - 1; x < allRecords.length; x++){
+display.innerHTML += "<tr><td>" + allRecords[x].firstname +"<\/td><td>" + allRecords[x].lastname + "<\/td>" + "<\/td><td>" + allRecords[x].age + btn + "<\/tr>" ;
+};  
+return false;
 });
+
+
+
