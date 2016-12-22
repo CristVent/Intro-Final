6c5c045e5f695a1a08ef0d0263bf908a00369797
Cristian Ventura <cristian101ventura@yahoo.com>
2016-12-22 10:34:44 -0500

    After code review with Jeff

--- action.js
+++ action.js
@@ -1,13 +1,21 @@
 
-var button = document.getElementById("add"),
+var addBtn = document.getElementById("addBtn"),
+    searchBtn = document.getElementById("searchBtn"),
     first = document.getElementById("firstname"),
     last = document.getElementById("lastname"),
     age = document.getElementById("age"),
-    display = document.getElementById("display-records");
+    gender = document.getElementById("gender"),
+    major = document.getElementById("major"),
+    displaySearch = document.getElementById("displaySearch"),
+    searchId =  document.getElementById("searchId"),
+    display = document.getElementById("display-records"),
+    recordTemplate = "<td>~firstName~</td><td>{{lastName}}</td>";
 
-var allRecords = [];
+var allRecords = [],
+    id = 0;
 
 var Record = function Record(first, last, age, gender, major){
+    this.idNum = id++;
     this.firstname = first;
     this.lastname = last;
     this.age = age;
@@ -15,26 +23,57 @@ var Record = function Record(first, last, age, gender, major){
     this.major = major;
 };
 
-var record1 = new Record("Cristian", "Ventura", "22", "male", "Computer Science"),
-    record2 = new Record("Joe", "Rogan", "34", "male", "Biology");
+allRecords.push(new Record("Cristian", "Ventura", "22", "male", "Computer"));
+allRecords.push(new Record("Joe", "Rogan", "34", "male", "Biology"));
+allRecords.push(new Record("Jim", "Norton", "40", "male", "Comedy"));
 
-allRecords.push(record1);
-allRecords.push(record2);
 
-var btn = "<td><button class=\"btn btn-xs btn-danger\"> Delete <\/button> <\/td>";
+var output = function output1(type){
+    var td =  "<\/td><td>";
+    /*
+    var startIndex = 0;
 
-for (var x = 0; x < allRecords.length; x++){
-display.innerHTML += "<tr><td>" + allRecords[x].firstname +"<\/td><td>" + allRecords[x].lastname + "<\/td>" + "<\/td><td>" + allRecords[x].age + btn + "<\/tr>";
+    if(type === 1) {
+        startIndex = allRecords.length - 1;
+    }
+
+    for(var i = startIndex; i < allRecords.length; i++){
+
+    }
+    */
+
+
+    if(type === 0){
+        for (var x = 0; x < allRecords.length; x++){
+            var curRec = allRecords[x];
+        display.innerHTML += "<tr><td>" + allRecords[x].idNum + td + allRecords[x].firstname + 
+            td + allRecords[x].lastname + td + allRecords[x].age + td + allRecords[x].gender + td + allRecords[x].major + "<\/tr>";
+            //display.innerHTML += recordTemplate.replace("{{firstName}}", allRecords[x].firstname).replace("{{lastname}}",allRecords[x].lastname);
+};
+    } else if (type === 1){
+         for (var x = allRecords.length - 1; x < allRecords.length; x++){
+         display.innerHTML += "<tr><td>" + allRecords[x].idNum + td + allRecords[x].firstname + 
+            td + allRecords[x].lastname + td + allRecords[x].age + td + allRecords[x].gender + td + allRecords[x].major + "<\/tr>";
+};  
+    }
 };
 
-button.addEventListener("click", function test(){
-   allRecords.push( new Record(first.value, last.value, age.value)); 
+output(0);
 
-   for (var x = allRecords.length - 1; x < allRecords.length; x++){
-display.innerHTML += "<tr><td>" + allRecords[x].firstname +"<\/td><td>" + allRecords[x].lastname + "<\/td>" + "<\/td><td>" + allRecords[x].age + btn + "<\/tr>" ;
-};  
-return false;
+addBtn.addEventListener("click", function test(){
+   allRecords.push( new Record(first.value, last.value, age.value, gender.value, major.value)); 
+   output(1);
 });
 
-
-
+var deleteBtn = document.getElementsByClassName("deleteBtn");
+searchBtn.addEventListener("click", function deleting(){
+    var searchIdValue = parseInt(searchId.value);
+    for( var i = 0; i < allRecords.length; i++){
+        if(searchIdValue == allRecords[i].idNum){   
+        allRecords.splice(i,1);
+        display.innerHTML = " ";
+        output(0);
+        break;
+    };
+};
+});
