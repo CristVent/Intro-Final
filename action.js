65ba4601614a9c393e483e1b52d53d4a8afc6c33
Cristian Ventura <cristian101ventura@yahoo.com>
2016-12-22 20:20:30 -0500

    Updated functions and cleaned code

--- action.js
+++ action.js
@@ -1,70 +1,60 @@
 
+// Defined the variables needed. Assigned elements from html elements to variables
 var addBtn = document.getElementById("addBtn"),
     searchBtn = document.getElementById("searchBtn"),
     first = document.getElementById("firstname"),
     last = document.getElementById("lastname"),
     age = document.getElementById("age"),
     gender = document.getElementById("gender"),
-    major = document.getElementById("major"),
+    year = document.getElementById("year"),
     displaySearch = document.getElementById("displaySearch"),
     searchId =  document.getElementById("searchId"),
     display = document.getElementById("display-records"),
-    recordTemplate = "<tr><td>{{id}}</td><td>{{firstName}}</td><td>{{lastName}}</td><td>{{age}}</td><td>{{gender}}</td><td>{{major}}</td></tr>";
+    recordTemplate = "<tr><td>{{id}}</td><td>{{firstName}}</td><td>{{lastName}}</td><td>{{age}}</td><td>{{gender}}</td><td>{{year}}</td></tr>";
 
+// Defined that array that will store all the objects 
 var allRecords = [],
     id = 0;
 
-var Record = function Record(first, last, age, gender, major){
+// Records object constructer 
+var Record = function Record(first, last, age, gender, year){
     this.idNum = id++;
     this.firstname = first;
     this.lastname = last;
     this.age = age;
     this.gender = gender;
-    this.major = major;
+    this.year = year;
 };
 
-allRecords.push(new Record("Cristian", "Ventura", "22", "male", "Computer"));
-allRecords.push(new Record("Joe", "Rogan", "34", "male", "Biology"));
-allRecords.push(new Record("Jim", "Norton", "40", "male", "Comedy"));
+// Build objects that will be displayed by defualt
+allRecords.push(new Record("Cristian", "Ventura", "22", "male", "Senior"));
+allRecords.push(new Record("Joe", "Brown", "23", "male", "Junior"));
+allRecords.push(new Record("Rose", "Norton", "21", "female", "Freshmen"));
 
 
+// All output to the table data will be processed
 var output = function output1(type){
-    var td =  "<\/td><td>";
-    /*
     var startIndex = 0;
-
-    if(type === 1) {
+    if(type === "add") {
         startIndex = allRecords.length - 1;
-    }
-
-    for(var i = startIndex; i < allRecords.length; i++){
-
-    }
-    */
-
-    if(type === 0){
-        for (var x = 0; x < allRecords.length; x++){
-            var allR = allRecords[x];
-        //display.innerHTML += "<tr><td>" + allRecords[x].idNum + td + allRecords[x].firstname + 
-           // td + allRecords[x].lastname + td + allRecords[x].age + td + allRecords[x].gender + td + allRecords[x].major + "<\/tr>";
-            display.innerHTML += recordTemplate.replace("{{id}}", allRecords[x].idNum).replace("{{firstName}}", allRecords[x].firstname).replace("{{lastName}}",allRecords[x].lastname)
-            .replace("{{age}}", allRecords[x].age).replace("{{gender}}", allRecords[x].gender).replace("{{major}}", allRecords[x].major);
-};
-    } else if (type === 1){
-         for (var x = allRecords.length - 1; x < allRecords.length; x++){
-         display.innerHTML += "<tr><td>" + allRecords[x].idNum + td + allRecords[x].firstname + 
-            td + allRecords[x].lastname + td + allRecords[x].age + td + allRecords[x].gender + td + allRecords[x].major + "<\/tr>";
-};  
+    };
+    for(var x = startIndex; x < allRecords.length; x++){
+        var allRec = allRecords[x];
+        display.innerHTML += recordTemplate.replace("{{id}}", allRecords[x].idNum).replace("{{firstName}}", allRecords[x].firstname).replace("{{lastName}}",allRecords[x].lastname)
+        .replace("{{age}}", allRecords[x].age).replace("{{gender}}", allRecords[x].gender).replace("{{year}}", allRecords[x].year);
     }
 };
 
-output(0);
+// First function call will be made to display the defualt records/ obects
+output();
 
+// New record/ object will be created on button click
 addBtn.addEventListener("click", function test(){
    allRecords.push( new Record(first.value, last.value, age.value, gender.value, major.value)); 
-   output(1);
+   output(add);
 });
 
+// Record/ object will a deleted on button click
 var deleteBtn = document.getElementsByClassName("deleteBtn");
 searchBtn.addEventListener("click", function deleting(){
     var searchIdValue = parseInt(searchId.value);
@@ -72,7 +62,7 @@ searchBtn.addEventListener("click", function deleting(){
         if(searchIdValue == allRecords[i].idNum){   
         allRecords.splice(i,1);
         display.innerHTML = " ";
-        output(0);
+        output();
         break;
     };
 };
